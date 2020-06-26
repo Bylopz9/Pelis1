45dc4c932f0c8726b878188ff7229207c5eb778e
Lopz_9 <manuel_lopez@live.com.mx>
2020-06-26 17:59:33 -0500

    Creacion de Template y codigo base

--- pelis/pelisapp/views.py
+++ pelis/pelisapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,"pelisapp/home.html")
+
+def contacto(request):
+    return render(request,"pelisapp/contacto.html")
+
+def capitulos(request):
+    return render(request,"pelisapp/cap.html")
