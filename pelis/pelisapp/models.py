5549a6ffaadd8c132d40a01d1ad464e60c2ca3d9
Lopz_9 <manuel_lopez@live.com.mx>
2020-06-26 19:14:32 -0500

    Creacion de las Bases de Datos

--- pelis/pelisapp/models.py
+++ pelis/pelisapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class generos(models.Model):
+
+    genero=models.models.CharField(max_length=50)
+
+class Peliculas(models.Model):
+    titulo=models.CharField(max_length=200)
+    duracion=models.TimeField()
+    anio=models.IntegerField()
+    genero=models.models.ForeignKey(generos, on_delete=models.CASCADE)
+    
